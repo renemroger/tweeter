470ee7b99ad5a45ee7ccd56529ffe8c20853cd06
mroger <rene.mroger@gmail.com>
2019-07-18 00:00:24 +0000

    dynamic tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
   const userData = [
@@ -10,7 +11,7 @@ $(document).ready(function() {
       "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-      "created_at": 1461116232227
+      "created_at": 1563518677227
     },
     {
       "user": {
@@ -25,34 +26,69 @@ $(document).ready(function() {
     }
   ]
 
-
-
   const createTweetElement = (data) => {
-    const newArticle = `
-  <article class="tweet">
-  <header class='tweet-header'>
-<p><img src="${data.user.avatars}" alt="Avatar" height="42" width="42">
-${data.user.name}<span class='floatRight hoverToggle'>${data.user.handle}</span></p>
-  </header>
-  <p class="tweet-text">${data.content.text}</p>
-  <footer class='tweet-footer'>
-    <span>${new Date(data.created_at)}</span>
-    <i class="glyphicon glyphicon-flag floatRight"></i>
-    <i class="glyphicon glyphicon-comment floatRight"></i>
-    <i class="glyphicon glyphicon-thumbs-up floatRight"></i>
-  </footer>
-</article>
-  `
-    return newArticle;
+    const $article = $('<article>').addClass('tweet');
+
+    const $myHeader = $('<header>').addClass("tweet-header");
+    const $headerContentP = $('<span>').text(`${data.user.name}`);
+    const $avatarIcon = $('<img>')
+      .attr('src', `${data.user.avatars}`)
+      .attr('alt', 'Avatar')
+      .attr('height', '42')
+      .attr('width', '42');
+
+    const $idSpan = $('<span>').addClass('floatRight hoverToggle').text(`${data.user.handle}`);
+    const $tweetText = $('<p>').addClass('tweet-text').text(`${data.content.text}`);
+    const $myFooter = $('<footer>').addClass('tweet-footer');
+
+    const $iconOne = $('<i>').addClass('glyphicon glyphicon-flag floatRight');
+    const $iconTWo = $('<i>').addClass('glyphicon glyphicon-comment floatRight');
+    const $iconThree = $('<i>').addClass('glyphicon glyphicon-thumbs-up floatRight');
+    const $createdAt = $('<span>').text(`${moment(new Date(data.created_at)).fromNow()}`);
+
+    $myHeader.append($headerContentP, $avatarIcon, $idSpan);
+    $myFooter.append($createdAt, $iconOne, $iconTWo, $iconThree);
+
+    $article.append($myHeader, $tweetText, $myFooter);
+
+    return $article;
   }
 
   const renderTweets = function(userData) {
+    $('.tweets-container').empty();
     for (const t of userData) {
       $('.tweets-container').append(createTweetElement(t));
 
     }
   }
-  renderTweets(userData);
+
+  const validateForm = (form) => {
+    return form || form.length < 10 ? false : true;
+  }
+
+  $('.tweet-form').submit((events) => {
+    events.preventDefault();
+    if (validateForm($('.tweet-form').serialize())) {
+      $.post('/tweets', $('.tweet-form').serialize(), (newPost) => {
+        loadTweets();
+      });
+    } else {
+      alert('bad input')
+    }
+  })
+
+  const loadTweets = function() {
+    $.getJSON('/tweets')
+      .done((arrayOfPosts) => {
+        renderTweets(arrayOfPosts);
+      })
+      .fail((error) => {
+        console.log(error);
+      });
+  }
+
+  loadTweets();
+
 });
 
 
--- public/scripts/tweetContainer.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.tweet').hover(function() {
-
-    $($('.hoverToggle', this)).css("visibility", "visible");
-  }, (function() {
-    $($('.hoverToggle', this)).css("visibility", "hidden");
-  }));
-});
